e23dfe963ed0a3d1038bca827379c093de5c5c66
Raja-mishra1 <rajamishra0702@gmail.com>
2021-10-02 22:18:24 +0530

    [ADD]: added urls to account and views

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from .views import RegisterAPI
+from django.urls import path
+from knox import views as knox_views
+from .views import LoginAPI
+
+app_name = 'account'
+
+urlpatterns = [
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+]
